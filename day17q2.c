78c0d051032f4c891aa4edf88ef1f912c46fcdbc
harsh24091975 <74312669+harsh24091975@users.noreply.github.com>
2026-09-10 23:32:30 +0530

    Add files via upload

---
+++ day17q2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, prime = 1;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    if(n <= 1) {
+        prime = 0;
+    }
+
+    for(i = 2; i < n; i++) {
+        if(n % i == 0) {
+            prime = 0;
+            break;
+        }
+    }
+
+    if(prime == 1) {
+        printf("Prime number");
+    }
+    else {
+        printf("Not a prime number");
+    }
+
+    return 0;
+}
