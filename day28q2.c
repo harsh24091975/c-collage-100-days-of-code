68fff8fcc1f2a7a408cc455ab360fccbe3cd3630
harsh24091975 <74312669+harsh24091975@users.noreply.github.com>
2026-09-24 22:28:29 +0530

    Add files via upload

---
+++ day28q2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
